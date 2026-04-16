5d6c6ac7eb437ef3e7930924b3d5dfa2cfd38ea1
Quoc Truong <truongquoc2k5@gmail.com>
2026-04-16 20:23:56 +0700

    ud

--- ontap/th4.py
+++ ontap/th4.py
@@ -1,16 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
-
 anh = cv2.imread('data/quoc.jpg')
 anhxam = cv2.cvtColor(anh,6)
-
 loctv = cv2.medianBlur(anhxam,5)
-
-otsu,nhiphan = cv2.threshold(loctv,1,255,0+8)
-
-contours,_ = cv2.findContours(nhiphan,0,2)
-
+adaptive = cv2.adaptiveThreshold(loctv,255,0,0,15,2)
+otsu , nhiphan = cv2.threshold(loctv,1,255,0+8)
+contours , _ = cv2.findContours(nhiphan,0,2)
 cv2.drawContours(anh,contours,-1,(0,0,255),2)
-
-plt.subplot(111),plt.imshow(cv2.cvtColor(anh,4))
+plt.subplot(121),plt.imshow(cv2.cvtColor(anh,4))
+plt.subplot(122),plt.imshow(cv2.cvtColor(adaptive,4))
 plt.show()
