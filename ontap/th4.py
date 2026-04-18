6f4296d646fab8d9de522995b0cb8184061f5a43
Quoc Truong <truongquoc2k5@gmail.com>
2026-04-18 15:01:08 +0700

    ud

--- ontap/th4.py
+++ ontap/th4.py
@@ -1,12 +1,35 @@
+#bai5
 import cv2
-import matplotlib.pyplot as plt
+import numpy as np 
 anh = cv2.imread('data/quoc.jpg')
-anhxam = cv2.cvtColor(anh,6)
-loctv = cv2.medianBlur(anhxam,5)
-adaptive = cv2.adaptiveThreshold(loctv,255,0,0,15,2)
-otsu , nhiphan = cv2.threshold(loctv,1,255,0+8)
-contours , _ = cv2.findContours(nhiphan,0,2)
-cv2.drawContours(anh,contours,-1,(0,0,255),2)
-plt.subplot(121),plt.imshow(cv2.cvtColor(anh,4))
-plt.subplot(122),plt.imshow(cv2.cvtColor(adaptive,4))
-plt.show()
+cao,rong = anh.shape[:2]
+def ham(x):
+    dx = cv2.getTrackbarPos('dx','anh')
+    dy = cv2.getTrackbarPos('dy','anh')
+    matran = np.float32([[1,0,dx],[0,1,dy]])    
+    anhdichchuyen = cv2.warpAffine(anh,matran,(rong,cao))
+    cv2.imshow('anh',anhdichchuyen)
+cv2.namedWindow('anh')
+cv2.createTrackbar('dx','anh',10,100,ham)
+cv2.createTrackbar('dy','anh',10,100,ham)
+ham(0)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
+
+
+
+import cv2
+import numpy as np
+anh = cv2.imread('data/quoc.jpg')
+cao,rong = anh.shape[:2]
+matran = np.float32([[1,0,50],[0,1,0]])
+ketqua = cv2.warpAffine(anh,matran,(rong,cao))
+blur = cv2.medianBlur(ketqua,5)
+anhxam = cv2.cvtColor(blur,6)
+_,nhiphan = cv2.threshold(anhxam,127,255,0)
+
+
+cv2.imshow('anh',anh)           
+
