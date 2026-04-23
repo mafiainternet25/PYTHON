72470af241a3b4575338383985d4e237adbb6c76
Trương Như Quốc <truongquoc2k5@gmail.com>
2026-04-23 18:59:09 +0700

    Update cau3.py

--- ontap/cau3.py
+++ ontap/cau3.py
@@ -5,6 +5,7 @@
 # e. khử nhiễu ảnh bằng bộ lọc trung bình.
 # f. thực hiện phân ngưỡng ảnh bằng thuật toán phân ngưỡng thích nghi.
 # j. tách biên ảnh bằng phương pháp tách biên laplace.
+# h. khử nhiễu ảnh bằng bộ lọc song phương.
 # d. hiển thị ảnh ban đầu , các ảnh kết quả trên matplotlib.
 
 import cv2
@@ -19,24 +20,22 @@ _,otsu = cv2.threshold(blur,1,255,0+8)
 
 canny = cv2.Canny(anhxam,50,150)
 
-median = cv2.medianBlur(anhxam,5)
-
 adapt = cv2.adaptiveThreshold(blur,255,1,0,15,2)
 
 laplace = cv2.Laplacian(anhxam,6)
 
-
+bilateral = cv2.bilateralFilter(anhxam,15,15,15)
 
 plt.gray()
 plt.subplot(241),plt.imshow(cv2.cvtColor(anh,4))
 plt.subplot(242),plt.imshow(cv2.cvtColor(gauss,4))
 plt.subplot(243),plt.imshow(cv2.cvtColor(otsu,4))
 plt.subplot(244),plt.imshow(cv2.cvtColor(canny,4))
-plt.subplot(245),plt.imshow(cv2.cvtColor(median,4))
+plt.subplot(245),plt.imshow(cv2.cvtColor(blur,4))
 plt.subplot(246),plt.imshow(cv2.cvtColor(adapt,4))
 plt.subplot(247),plt.imshow(cv2.convertScaleAbs(laplace))
+plt.subplot(248),plt.imshow(cv2.cvtColor(bilateral,4))
 plt.show()
 
 
-cv2.waitKey(0)
-cv2.destroyAllWindows
+
